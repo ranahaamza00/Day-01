013764fbdb9f5b4c0dbb1a9458499ccd05e9f2e7
ranahaamza00 <ranahaamza00@gmail.com>
2024-04-24 04:27:12 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var name1 = "HAMZA";
+console.log("Hello ".concat(name1, ",  Would you like to learn some typescript today?"));
+//QUESTION 03 
+var name2 = "RANA";
+console.log(name2.toLowerCase());
+console.log(name2.toUpperCase());
+console.log(name2.charAt(0).toUpperCase() + name2.slice(1).toLowerCase());
---
+++ index.ts
@@ -0,0 +1,15 @@
+
+let name1: string = "HAMZA";
+console.log(`Hello ${name1},  Would you like to learn some typescript today?`);
+
+//QUESTION 03 
+
+let name2: string = "RANA";
+console.log(name2.toLowerCase());
+console.log(name2.toUpperCase());
+
+console.log(name2.charAt(0).toUpperCase() + name2.slice(1).toLowerCase());
+
+
+
+
